2afdab6b6be5ac1551e7a5747f93f15e3dcc1a74
CardenB <CardenB@Cardens-Mac-Pro.local>
2014-08-03 14:55:08 -0400

    Began work on the snapchat-style container view.

---
+++ Bucket List/ContainerView/BLMainViewContainer.h
@@ -0,0 +1,13 @@
+//
+//  BLMainViewContainer.h
+//  Bucket List
+//
+//  Created by Carden Bagwell on 8/3/14.
+//  Copyright (c) 2014 Carden Bagwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLMainViewContainer : UIScrollView
+
+@end
---
+++ Bucket List/ContainerView/BLMainViewContainer.m
@@ -0,0 +1,36 @@
+//
+//  BLMainViewContainer.m
+//  Bucket List
+//
+//  Created by Carden Bagwell on 8/3/14.
+//  Copyright (c) 2014 Carden Bagwell. All rights reserved.
+//
+
+#import "BLMainViewContainer.h"
+
+@implementation BLMainViewContainer
+
+- (id)initWithViewControllers:(NSArray *)viewControllers
+{
+    
+}
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        self.pagingEnabled = YES;
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
